f71e07e9e3f773671cfa70c6a38dbe9c39396fe7
annahadrinielle <72071218+annahadrinielle@users.noreply.github.com>
2021-06-30 12:43:56 +0530

    Update Boat.js

--- js/Boat.js
+++ js/Boat.js
@@ -1,4 +1,6 @@
 class Boat {
+  
+  //contsructor has an additional parameter boatAnimation
   constructor(x, y, width, height, boatPos, boatAnimation) {
     var options = {
       restitution: 0.8,
@@ -6,8 +8,12 @@ class Boat {
       density: 1.0,
       label: "boat"
     };
+    
+    //create a new property named animation to store boatAnimation value
     this.animation = boatAnimation;
+    //property to set animation speed
     this.speed = 0.05;
+    
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
     this.height = height;
@@ -17,6 +23,7 @@ class Boat {
     World.add(world, this.body);
   }
 
+  //set speed of animation
   animate() {
     this.speed += 0.05 % 1.1;
   }
@@ -30,12 +37,17 @@ class Boat {
   display() {
     var angle = this.body.angle;
     var pos = this.body.position;
+    
+    //create index by dividing speed by length of animation and getting the smallest value by floor() function
+    //index will be used to traverse through the set of animations
     var index = floor(this.speed % this.animation.length);
     
     push();
     translate(pos.x, pos.y);
     rotate(angle);
     imageMode(CENTER);
+    
+    //use index to get the animation
     image(this.animation[index], 0, this.boatPosition, this.width, this.height);
     noTint();
     pop();
